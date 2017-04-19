8225c40ff652229f29123031877ae39259eed50f
Fred Akalin <akalin@keyba.se>
2017-04-18 17:45:23 -0700

    Add test for decrypting unknown minor versions (#31)

--- decrypt_test.go
+++ decrypt_test.go
@@ -7,9 +7,9 @@ import "testing"
 
 func TestVersionValidator(t *testing.T) {
 	plaintext := []byte{0x01}
-	sndr := newBoxKey(t)
+	sender := newBoxKey(t)
 	receivers := []BoxPublicKey{newBoxKey(t).GetPublicKey()}
-	ciphertext, err := Seal(Version1(), plaintext, sndr, receivers)
+	ciphertext, err := Seal(Version1(), plaintext, sender, receivers)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -18,4 +18,35 @@ func TestVersionValidator(t *testing.T) {
 	if err == nil {
 		t.Fatal("Unexpected nil error")
 	}
+}
+
+func testNewMinorVersion(t *testing.T, version Version) {
+	plaintext := []byte{0x01}
+
+	newVersion := version
+	newVersion.Minor++
+
+	teo := testEncryptionOptions{
+		corruptHeader: func(eh *EncryptionHeader) {
+			eh.Version = newVersion
+		},
+	}
+	sender := newBoxKey(t)
+	receivers := []BoxPublicKey{newBoxKey(t).GetPublicKey()}
+	ciphertext, err := testSeal(version, plaintext, sender, receivers, teo)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, _, err = Open(SingleVersionValidator(newVersion), ciphertext, kr)
+	if err != nil {
+		t.Fatalf("Unepected error %v", err)
+	}
+}
+
+func TestDecrypt(t *testing.T) {
+	tests := []func(*testing.T, Version){
+		testNewMinorVersion,
+	}
+	runTestsOverVersions(t, "test", tests)
 }

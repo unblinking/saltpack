d453e28f1b04e1df3321a7db4026b2a702afe3ee
M Mou <mmou@keyba.se>
2020-02-28 10:38:31 -0800

    fix

--- errors.go
+++ errors.go
@@ -126,7 +126,7 @@ func (e ErrNoSenderKey) Error() string {
 	return "no sender key found for message"
 }
 func (e ErrWrongMessageType) Error() string {
-	return fmt.Sprintf("Wrong saltpack message type: wanted %s, but got %s instead", e.wanted, e.received)
+	return fmt.Sprintf("Wrong saltpack message type: wanted %s, but got %s instead", e.Wanted, e.Received)
 }
 func (e ErrBadVersion) Error() string {
 	return fmt.Sprintf("Unsupported version (%s)", e.received)
